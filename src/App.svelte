ef25d699bee2a8320c47eb7c0abc498dcb3fd095
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2023-05-18 10:47:02 +0100

    updated about section

--- src/App.svelte
+++ src/App.svelte
@@ -137,7 +137,7 @@
   </div>
   {/if}
 
-  <p style:max-width="780px">This tool was coded by <a href="https://ahmadbarclay.com/">Ahmad Barclay</a> based on geography files available from the <a href="https://geoportal.statistics.gov.uk/">ONS Open Geography Portal</a>. You can find the code used to generate the TopoJSON file in <a href="https://github.com/onsvisual/uk-topojson">this Github repo</a>.</p>
+  <p style:max-width="780px"><small>This tool was coded by <a href="https://ahmadbarclay.com/">Ahmad Barclay</a> based on geography files available from the <a href="https://geoportal.statistics.gov.uk/">ONS Open Geography Portal</a>. You can find the code used to generate the TopoJSON file in <a href="https://github.com/onsvisual/uk-topojson">this Github repo</a>.</small></p>
 </main>
 
 <style>
